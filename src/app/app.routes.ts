bb96faf49bbf7687766034451d0303cf1ed3aca6
Ashtam Katiyar <katiyarashtam014@gmail.com>
2026-06-10 14:28:17 +0530

    Updated auth components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login-component/login-component.component';
+import { RegisterComponent } from './components/auth/register-component/register-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
